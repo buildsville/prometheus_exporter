fb177b85d63c4e4ac093edb8ad16cc17ff18ecb6
masahata <buildsville.lab@gmail.com>
2018-03-25 11:09:23 +0900

    simply fd metric

---
+++ lib/prometheus_exporter/metric/fd_metric.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module PrometheusExporter::Metric
+  class FdMetric < Base
+    attr_reader :data
+
+    def initialize(name, help)
+      super
+      @data = Hash.new { |label,category| label[category] = {} }
+    end
+
+    def type
+      "fd_metric"
+    end
+
+    def metric_text
+      text = String.new
+      first = true
+      @data.each do |labels, values|
+        text << "\n" unless first
+        first = false
+        text << "#{prefix(@name)}_sum#{labels_text(labels)} #{@data[labels][:sum]}\n"
+        text << "#{prefix(@name)}_count#{labels_text(labels)} #{@data[labels][:count]}"
+      end
+      text
+    end
+
+    def observe(value, labels = {})
+      @data[labels][:sum] ||= 0
+      @data[labels][:count] ||= 0
+
+      @data[labels][:sum] += value
+      @data[labels][:count] += 1
+    end
+  end
+end
